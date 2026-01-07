8492024c8e7617932c77e9a3cc80fff428ba4350
Cristobal Gallego-Castillo <83344529+cristobal-GC@users.noreply.github.com>
2026-01-07 16:59:28 +0100

    ignore pycache

--- src/config/parameters.py
+++ src/config/parameters.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 
 def load_scenario(scenario_name="base_case"):
-    data_path = Path(__file__).resolve().parents[2] / "data" / "scenarios"
-    scenario_file = data_path / f"{scenario_name}.yaml"
+    scenario_path = Path(__file__).resolve().parents[2] / "data" / "scenarios"
+    scenario_file = f"{scenario_name}.yaml"
 
-    with open(scenario_file, "r") as f:
+    with open(scenario_path / scenario_file, "r") as f:
         scenario = yaml.safe_load(f)
 
     return scenario
